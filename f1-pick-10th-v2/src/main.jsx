290990a972834c1871f5f2103909e0ebbf5e0596
Lechefrio <reggieritt702@gmail.com>
2026-05-17 13:16:01 -0700

    Load player avatar styles

--- f1-pick-10th-v2/src/main.jsx
+++ f1-pick-10th-v2/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./styles/app.css";
 import "./styles/v2-polish.css";
 import "./styles/race-visual.css";
+import "./styles/player-avatars.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
