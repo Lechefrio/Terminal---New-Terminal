665e0c6b082b19a28395d8d3eb78082cd0616be3
Lechefrio <reggieritt702@gmail.com>
2026-05-17 11:30:43 -0700

    Import race visual stylesheet

--- f1-pick-10th-v2/src/main.jsx
+++ f1-pick-10th-v2/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/app.css";
 import "./styles/v2-polish.css";
+import "./styles/race-visual.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
